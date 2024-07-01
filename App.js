8a776648575e526cba2194573d76c7a415c8473d
sangbmorbenjamin <bsangbmor001@st.ug.edu.gh>
2024-07-01 02:02:23 +0000

    added my name Sangbmor Benjamin

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>My name is <Text style={styles.boldText}>Sangbmor Benjamin</Text></Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'yellow',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text:{
+    fontSize: 24,
+  },
+  boldText:{
+    fontWeight: 'bold',
   },
 });
